67bb2f833eb7c083bf6680d9e4266751523b1739
Daniel Hsing <yenh@wepay.com>
2023-08-21 15:52:11 -0400

    init repo with reducer
    Signed-off-by: Daniel Hsing <yenh@wepay.com>

---
+++ src/store/reducer.ts
@@ -0,0 +1,78 @@
+import {
+    ActionReducerMapBuilder,
+    PayloadAction,
+    SliceCaseReducers,
+    ValidateSliceCaseReducers,
+    createSlice,
+} from "@reduxjs/toolkit";
+
+type Dispute = {
+    createTime: Date;
+    id: string;
+    amount: number;
+    status: DisputeStatusEnum;
+};
+
+enum DisputeStatusEnum {
+    Open = 0,
+    Challenged = 1,
+    Condeded = 2,
+}
+
+export type DisputeReducerState = {
+    disputes: Dispute[];
+};
+
+const initialState: DisputeReducerState = {
+    disputes: [],
+};
+
+type ConfigReducerOpts = {
+    actionOverrides: ValidateSliceCaseReducers<
+        DisputeReducerState,
+        SliceCaseReducers<DisputeReducerState>
+    >;
+    initialState: DisputeReducerState;
+    extraActions: (
+        builder: ActionReducerMapBuilder<DisputeReducerState>
+    ) => void;
+};
+
+export function configureReducer(
+    opts: ConfigReducerOpts = {
+        initialState,
+        actionOverrides: {},
+        extraActions: () => {},
+    }
+) {
+    return createSlice({
+        name: "dispute",
+        initialState,
+        reducers: {
+            addDispute(state, action: PayloadAction<Dispute>) {
+                state.disputes.push(action.payload);
+            },
+            setDisputes(state, action: PayloadAction<Dispute[]>) {
+                state.disputes = action.payload;
+            },
+            concedeDispute(state, action: PayloadAction<string>) {
+                const dispute = state.disputes.find(
+                    (d) => d.id === action.payload
+                );
+                if (dispute && dispute.status === DisputeStatusEnum.Open)
+                    dispute.status = DisputeStatusEnum.Condeded;
+            },
+            challengeDispute(state, action: PayloadAction<string>) {
+                const dispute = state.disputes.find(
+                    (d) => d.id === action.payload
+                );
+                if (dispute && dispute.status === DisputeStatusEnum.Open)
+                    dispute.status = DisputeStatusEnum.Challenged;
+            },
+            ...opts.actionOverrides,
+        },
+        extraReducers: opts.extraActions,
+    });
+}
+
+export default configureReducer();
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+const { DefinePlugin } = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = (env, argv) => {
+    const mode = argv.mode || process.env.NODE_ENV || "development";
+    return {
+        mode,
+        module: {
+            rules: [
+                {
+                    test: /\.(js|jsx|ts|tsx)$/,
+                    exclude: /node_modules/,
+                    use: ["babel-loader"],
+                },
+                {
+                    test: /\.css$/i,
+                    use: [
+                        MiniCssExtractPlugin.loader,
+                        {
+                            loader: require.resolve("css-loader"),
+                            options: {
+                                url: false, // Required as image imports should be handled via JS/TS import statements
+                            },
+                        },
+                    ],
+                },
+                {
+                    test: /\.svg$/i,
+                    type: "asset",
+                    resourceQuery: /url/, // *.svg?url
+                },
+                {
+                    test: /\.(gif|jpg|png)$/,
+                    type: "asset/resource",
+                },
+            ],
+        },
+        entry: {
+            main: "./src/index.tsx",
+            reducer: "./src/store/reducer.ts",
+        },
+        output: {
+            path: path.resolve(__dirname, "build"),
+            filename: "[name]/[name].js",
+            chunkFilename: "[name].js",
+        },
+        resolve: {
+            extensions: ["*", ".js", ".jsx", ".ts", ".tsx"],
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: "./src/index.html",
+            }),
+
+            new DefinePlugin({
+                "process.env.NODE_ENV": JSON.stringify(mode),
+            }),
+            new MiniCssExtractPlugin(),
+        ],
+        devServer: {
+            static: {
+                directory: path.join(__dirname, "dist"),
+            },
+            hot: true,
+        },
+    };
+};
